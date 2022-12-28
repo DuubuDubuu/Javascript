629f39e49f9e878360d6292f47634faced112a9e
Lance Montiague <105115967+DuubuDubuu@users.noreply.github.com>
2022-12-28 16:34:53 +0800

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,12 @@
+const person = {
+  firstName: 'lance',
+  lastName: 'montiague',
+  age: 30,
+  hobbies: ['esports', 'cooking', 'driving'],
+  address: {
+    city: 'Marikina',
+    region: 'nCR'
+  }
+}
+
+console.log(person.firstName,person.hobbies);
